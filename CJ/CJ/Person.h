661a6920f2a6eaf63bad182ba871cc253efd0010
qianfeng <qianfeng@568.local>
2016-03-24 14:52:52 +0800

    新建

---
+++ CJ/CJ/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  CJ
+//
+//  Created by qianfeng on 16/3/24.
+//  Copyright © 2016年 LCJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ CJ/CJ/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  CJ
+//
+//  Created by qianfeng on 16/3/24.
+//  Copyright © 2016年 LCJ. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
