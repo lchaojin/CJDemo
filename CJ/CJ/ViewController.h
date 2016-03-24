15fe2e2f807f24de7bead887c7fdd8285b476c24
qianfeng <qianfeng@568.local>
2016-03-24 14:47:27 +0800

    新建工程

---
+++ CJ/CJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJ
+//
+//  Created by qianfeng on 16/3/24.
+//  Copyright © 2016年 LCJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CJ/CJ/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CJ
+//
+//  Created by qianfeng on 16/3/24.
+//  Copyright © 2016年 LCJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
